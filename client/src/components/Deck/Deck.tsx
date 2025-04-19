152ff32daf7c9d1cf08832322dbe180af6bd710c
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-04-18 20:35:12 -0400

    added a Deck component to drop the pokemon

--- client/src/components/Deck/Deck.tsx
+++ client/src/components/Deck/Deck.tsx
@@ -4,7 +4,174 @@ function Deck() {
   return (
     <div className="deck-container">
       <h1>Deck</h1>
-      <div className="pokemon-deck"></div>
+      <div className="pokemon-deck">
+        <ul className="deck">
+          <li data-swapy-slot="1"></li>
+          <li data-swapy-slot="2">
+            <div data-swapy-item="item-2">wefwe</div>
+          </li>
+          <li data-swapy-slot="3">
+            <div data-swapy-item="item-3">fewfwe</div>
+          </li>
+          <li data-swapy-slot="4"></li>
+          <li data-swapy-slot="5"></li>
+          <li data-swapy-slot="6"></li>
+          <li data-swapy-slot="7"></li>
+          <li data-swapy-slot="8"></li>
+          <li data-swapy-slot="9"></li>
+          <li data-swapy-slot="10"></li>
+          <li data-swapy-slot="11">
+            <div data-swapy-item="item-11"></div>
+          </li>
+          <li data-swapy-slot="12">
+            <div data-swapy-item="item-12"></div>
+          </li>
+          <li data-swapy-slot="13">
+            <div data-swapy-item="item-13"></div>
+          </li>
+          <li data-swapy-slot="14">
+            <div data-swapy-item="item-14"></div>
+          </li>
+          <li data-swapy-slot="15">
+            <div data-swapy-item="item-15"></div>
+          </li>
+          <li data-swapy-slot="16">
+            <div data-swapy-item="item-16"></div>
+          </li>
+          <li data-swapy-slot="17">
+            <div data-swapy-item="item-17"></div>
+          </li>
+          <li data-swapy-slot="18">
+            <div data-swapy-item="item-18"></div>
+          </li>
+          <li data-swapy-slot="19">
+            <div data-swapy-item="item-19"></div>
+          </li>
+          <li data-swapy-slot="20">
+            <div data-swapy-item="item-20"></div>
+          </li>
+          <li data-swapy-slot="21">
+            <div data-swapy-item="item-21"></div>
+          </li>
+          <li data-swapy-slot="22">
+            <div data-swapy-item="item-22"></div>
+          </li>
+          <li data-swapy-slot="23">
+            <div data-swapy-item="item-23"></div>
+          </li>
+          <li data-swapy-slot="24">
+            <div data-swapy-item="item-24"></div>
+          </li>
+          <li data-swapy-slot="25">
+            <div data-swapy-item="item-25"></div>
+          </li>
+          <li data-swapy-slot="26">
+            <div data-swapy-item="item-26"></div>
+          </li>
+          <li data-swapy-slot="27">
+            <div data-swapy-item="item-27"></div>
+          </li>
+          <li data-swapy-slot="28">
+            <div data-swapy-item="item-28"></div>
+          </li>
+          <li data-swapy-slot="29">
+            <div data-swapy-item="item-29"></div>
+          </li>
+          <li data-swapy-slot="30">
+            <div data-swapy-item="item-30"></div>
+          </li>
+          <li data-swapy-slot="31">
+            <div data-swapy-item="item-31"></div>
+          </li>
+          <li data-swapy-slot="32">
+            <div data-swapy-item="item-32"></div>
+          </li>
+          <li data-swapy-slot="33">
+            <div data-swapy-item="item-33"></div>
+          </li>
+          <li data-swapy-slot="34">
+            <div data-swapy-item="item-34"></div>
+          </li>
+          <li data-swapy-slot="35">
+            <div data-swapy-item="item-35"></div>
+          </li>
+          <li data-swapy-slot="36">
+            <div data-swapy-item="item-36"></div>
+          </li>
+          <li data-swapy-slot="37">
+            <div data-swapy-item="37"></div>
+          </li>
+          <li data-swapy-slot="38">
+            <div data-swapy-item="38"></div>
+          </li>
+          <li data-swapy-slot="39">
+            <div data-swapy-item="39"></div>
+          </li>
+          <li data-swapy-slot="40">
+            <div data-swapy-item="40"></div>
+          </li>
+          <li data-swapy-slot="41">
+            <div data-swapy-item="41"></div>
+          </li>
+          <li data-swapy-slot="42">
+            <div data-swapy-item="42"></div>
+          </li>
+          <li data-swapy-slot="43">
+            <div data-swapy-item="43"></div>
+          </li>
+          <li data-swapy-slot="44">
+            <div data-swapy-item="44"></div>
+          </li>
+          <li data-swapy-slot="45">
+            <div data-swapy-item="45"></div>
+          </li>
+          <li data-swapy-slot="46">
+            <div data-swapy-item="46"></div>
+          </li>
+          <li data-swapy-slot="47">
+            <div data-swapy-item="47"></div>
+          </li>
+          <li data-swapy-slot="48">
+            <div data-swapy-item="48"></div>
+          </li>
+          <li data-swapy-slot="49">
+            <div data-swapy-item="49"></div>
+          </li>
+          <li data-swapy-slot="50">
+            <div data-swapy-item="50"></div>
+          </li>
+          <li data-swapy-slot="51">
+            <div data-swapy-item="51"></div>
+          </li>
+          <li data-swapy-slot="52">
+            <div data-swapy-item="52"></div>
+          </li>
+          <li data-swapy-slot="53">
+            <div data-swapy-item="53"></div>
+          </li>
+          <li data-swapy-slot="54">
+            <div data-swapy-item="54"></div>
+          </li>
+          <li data-swapy-slot="55">
+            <div data-swapy-item="55"></div>
+          </li>
+          <li data-swapy-slot="56">
+            <div data-swapy-item="56"></div>
+          </li>
+          <li data-swapy-slot="57">
+            <div data-swapy-item="57"></div>
+          </li>
+          <li data-swapy-slot="58">
+            <div data-swapy-item="58"></div>
+          </li>
+          <li data-swapy-slot="59">
+            <div data-swapy-item="59"></div>
+          </li>
+          <li data-swapy-slot="60">
+            <div data-swapy-item="60"></div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
