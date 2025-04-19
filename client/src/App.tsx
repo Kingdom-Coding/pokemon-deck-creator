99035b3420c865e717f174f8f1486fdada9ebbcd
Kingdom-Coding <jonathanv2283219@gmail.com>
2025-04-18 20:36:06 -0400

    implimented swapy so pokemon can be dropped from PokemonList component to Deck component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,41 @@
 import "./App.css";
 /* import Navbar from "./components/Navbar/Navbar"; */
-import { PokemonProvider } from "./hook/PokemonData";
+import { PokemonProvider, usePokemonData } from "./hook/PokemonData";
 import PokemonList from "./components/PokemonList/PokemonList";
 import Deck from "./components/Deck/Deck";
+import { useEffect, useRef } from "react";
+import { Swapy, createSwapy, BeforeSwapEvent } from "swapy";
+
 function App() {
+  const { pokemon } = usePokemonData();
+  const swapyRef = useRef<Swapy | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current || pokemon.length === 0) return;
+    if (!containerRef.current) return;
+
+    if (containerRef.current) {
+      swapyRef.current = createSwapy(containerRef.current, {
+        animation: "dynamic",
+      });
+      swapyRef.current?.onBeforeSwap((e: BeforeSwapEvent) => {
+        console.log("before swap:", e);
+        return true;
+      });
+    }
+
+    swapyRef.current?.enable(true);
+
+    return () => {
+      swapyRef.current?.destroy();
+    };
+  }, [pokemon]);
+
   return (
     <PokemonProvider>
       <h1>Pokemon</h1>
-      <div className="container">
+      <div className="container" ref={containerRef}>
         <Deck />
         <PokemonList />
       </div>
